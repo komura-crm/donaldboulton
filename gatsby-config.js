980269aa1eec553e38da7f3f08d5ce59ca9dba9d
komura-crm <a363380261@sina.com>
2021-01-04 09:33:54 +0800

    clean build bibwoe mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       }
     ],
     sidebarConfig: {
-      forcedNavOrder: ["/introduction", "/codeblock"],
+      forcedNavOrder: ["/introduction", "/enoch", "/posts"],
       ignoreIndex: true
     }
   },
