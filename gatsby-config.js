b167981c7eb5316b0b9c2c90fa215d156e4bc72d
komura-crm <a363380261@sina.com>
2021-03-06 10:19:20 +0800

    path error

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     name: `Bibwoe`,
     author: `Donald Boulton`,
     siteUrl: `https://bibwoe.com`,
+    pathPrefix: `/`,
     language: `en-US`,
     description: `Basic Instructions Books While On Earth`,
     social: [
